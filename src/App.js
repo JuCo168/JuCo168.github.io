1f413e8f2e3a2276a181c7d6bb3be1bdf8677570
Julius <jco@ucsd.edu>
2023-01-10 01:45:53 -0700

    added workflow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Let's see if it reloads
         </p>
         <a
           className="App-link"
