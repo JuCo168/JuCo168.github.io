86113486f42b3b01e471acbbc2b93c4b3dc63835
Julius <jco@ucsd.edu>
2023-01-31 01:19:05 -0800

    Using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from './components/Home'
 import About from './components/About'
 import Navbar from "./components/Navbar";
 import Projects from './components/Projects'
@@ -6,12 +8,18 @@ import Skills from './components/Skills'
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-    </main>
+    <Router>
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <Routes>
+        <Route path="/" element={Home}/>
+          <Route path="/about" element={About}/>
+          <Route path="/projects" element={Projects}/>
+          <Route path="/skills" element={Skills}/>
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
