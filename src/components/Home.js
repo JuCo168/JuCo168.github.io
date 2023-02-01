383fb1ce9d3a5deb2b6c961dfbb3d5dabc789d8b
Julius <jco@ucsd.edu>
2023-01-31 20:05:59 -0800

    Removed remaining href

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -16,16 +17,7 @@ function Home() {
               Laborum, voluptas natus?
             </p>
             <div className="flex justify-center">
-              <a
-                href="#contact"
-                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                Work With Me
-              </a>
-              <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                See My Past Work
-              </a>
+            <Link to="/projects">Projects</Link>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
