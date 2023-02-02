cbc874ff79f131156eb422adf340def72187d3c5
Julius <jco@ucsd.edu>
2023-02-01 21:32:03 -0800

    Working on navbar

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import close from './close.svg'
+import doggo_full from './doggo-full.png'
+import doggo_small from './doggo-small.png'
+import menu from './menu.svg'
+
+
+export {
+    close,
+    doggo_full,
+    doggo_small,
+    menu
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import { react, useStateHook } from 'react';
-import { Link } from 'react-router-dom';
-import Doggo from '../assets/doggo-small.png';
+import { react, useState } from 'react';
+import { close, doggo_small, menu } from '../assets';
 import { navLinks } from '../assets/constants';
 
 function Navbar() {
+    const [toggle, setToggle] = useState(false)
+
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
-            <img src={ Doggo } alt="doggo"
+            <img src={ doggo_small } alt="doggo"
             className='w-[64px] h-[64px]'/>
-            {/* Hidden for desktop */}
             <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
                 {navLinks.map((nav, index) => (
                     <li key={nav.id}
@@ -19,6 +19,30 @@ function Navbar() {
                     </li>
                 ))}
             </ul>
+
+            <div className="sm:hidden flex flex-1 justify-end items-center">
+                <img 
+                    src={toggle ? close : menu }
+                    alt="menu"
+                    className="w-[28px] h-[28px] object-contain"
+                    onClick={() => setToggle((prev) => !prev)}
+                />
+
+                <div
+                    className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+                >
+                    <ul className='list-none flex flex-col justify-end items-center flex-1'>
+                        {navLinks.map((nav, index) => (
+                            <li key={nav.id}
+                            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}>
+                                <a href={`#${nav.id}`}>
+                                    {nav.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </nav>
   );
 }
