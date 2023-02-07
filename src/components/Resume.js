9198998719da525cf76ee7d15821cc3340d37481
Julius <jco@ucsd.edu>
2023-02-07 01:39:05 -0800

    Added PDF scaling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,19 +1,22 @@
+import { useState, useEffect } from 'react';
 import styles from "../styles";
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink = 'https://raw.githubusercontent.com/JuCo168/JuCo168.github.io/main/src/assets/Resume%202.2.pdf'
 
 export default function Resume() {
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <Document file={resumeLink} 
-        style={{
-          height: '200vh',
-          width: '200vw',
-          margin: 'auto',
-        }}>
-          <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
-        </Document>
-      </div>
-    );
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center mx-auto">
+      <Document file={resumeLink} >
+        <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} scale={width > 768 ? 1.4 : 0.6}/>
+      </Document>
+    </div>
+  );
 }
