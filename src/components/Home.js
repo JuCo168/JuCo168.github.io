830a3408f64a992d4b248f6964b5800860cf7015
Julius <jco@ucsd.edu>
2023-02-11 16:26:15 -0800

    Dependency cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,22 +10,14 @@ import { slides } from "../assets";
 function Home() {
   return (
     <section id="home" className={`flex justify-center flex-col ${styles.paddingY}`}>
-      <Swiper
-        slidesPerView={1}
+      <Swiper slidesPerView={1}
         spaceBetween={0}
-        autoplay={{
-          delay: 1700,
-          disableOnInteraction: false,
-        }}
+        autoplay={{ delay: 1700, disableOnInteraction: false, }}
         loop={true}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
+        pagination={{ clickable: true, dynamicBullets: true, }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        className="w-full h-full mx-auto"
-      >
+        className="w-full h-full mx-auto">
         { slides.map((slide, index) => (
           <SwiperSlide className={`block object-cover w-full h-full ${slide.pos}-[${slide.px}px]`}>
             <img src={slide.image} alt={slide.desc} key={index} />
