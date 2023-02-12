cedd8ba445ac68b6883ffa11ffea2657cb5b65f4
Julius <jco@ucsd.edu>
2023-02-11 16:00:48 -0800

    Finally a good carousel!

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,28 +21,24 @@ export {
 export const slides = [
     {
         image: bundestag,
-        label: 'Reichstag Building',
         desc: 'The Reichstag building in Berlin!',
         pos: 'bottom',
         px: 70
     },
     {
         image: gundam,
-        label: 'RX-79-2',
-        desc: 'From a Bandai pop-up store in San Diego',
+        desc: 'RX-79-2 from a Bandai pop-up store in San Diego',
         pos: 'bottom',
         px: 50
     },
     {
         image: pc,
-        label: 'My custom-built PC',
         desc: 'NCASE M1 | 5600X | EVGA FTW3 3060Ti',
         pos: 'top',
         px: 75
     },
     {
         image: peiwenie,
-        label: 'My partner',
         desc: '@peiwenie!',
         pos: 'bottom',
         px: 0
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,37 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay, Navigation } from "swiper";
+import 'swiper/css';
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import styles from "../assets/styles"
 import { slides } from "../assets";
-import { SlArrowLeft, SlArrowRight } from 'react-icons/sl'
-import Carousel from "./Carousel";
-import Swipe from "react-easy-swipe";
 
 function Home() {
-  const [curIndex, setIndex] = useState(0)
-  const [pause, setPause] = useState(false)
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (pause === false) {
-        setIndex(curIndex => (curIndex + 1) % slides.length);
-      }}, 1500);
-
-    return () => clearInterval(intervalId);
-  });
-  const nextSlide = () => {
-    return setIndex(curIndex => (curIndex + 1) % slides.length)
-  }
-  const prevSlide = () => {
-    return setIndex(curIndex === 0 ? slides.length - 1 : curIndex - 1)
-  }
-
   return (
     <section id="home" className={`flex justify-center flex-col ${styles.paddingY}`}>
-      <div className="w-full h-[600px] flex overflow-hidden relative items-center justify-center">
-        <Swipe onSwipeLeft={prevSlide} onSwipeRight={nextSlide}>
-          { slides.map((slide, index) => (
-            <div className={ index === curIndex ? `object-cover relative ${slide.pos}-[${slide.px}px]` : 'hidden' }>
-              <img src={slide.image} alt={slide.label} key={index} 
-                onMouseEnter={() => { setPause(true) }}
-                onMouseLeave={() => { setPause(false) }}/>
-            </div>
-          ))}
-        </Swipe>
-        {/* slide indicators */}
-        <div className="absolute w-full flex justify-center bottom-0">
-          {slides.map((_, index) => (
-            <div className={ index === curIndex
-                ? "h-2 w-2 bg-tertiary rounded-full mx-2 mb-2 cursor-pointer"
-                : "h-2 w-2 bg-white rounded-full mx-2 mb-2 cursor-pointer" }
-              key={index} onClick={() => { setIndex(index) }}/>
-          ))}
-        </div>
-        {/* left and right buttons */}
-        <SlArrowLeft onClick={prevSlide} className='absolute left-0 text-3xl inset-y-1/2 text-white cursor-pointer' />
-        <SlArrowRight onClick={nextSlide} className='absolute right-0 text-3xl inset-y-1/2 text-white cursor-pointer' />
-      </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={0}
+        autoplay={{
+          delay: 1700,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="w-full h-full mx-auto"
+      >
+        { slides.map((slide, index) => (
+          <SwiperSlide className={`block object-cover w-full h-full ${slide.pos}-[${slide.px}px]`}>
+            <img src={slide.image} alt={slide.desc} key={index} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
   
